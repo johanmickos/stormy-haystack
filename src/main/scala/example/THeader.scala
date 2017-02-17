f63014309e8515915e7f5f213b87478454c6f26d
Johan Mickos <jmickos@bu.edu>
2017-02-17 16:27:37 +0100

    Remove example code

--- src/main/scala/example/THeader.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-import se.sics.kompics.network.{Header, Transport}
-
-final case class THeader(src: TAddress, dst: TAddress, proto: Transport) extends Header[TAddress] {
-  override def getSource: TAddress = src
-  override def getDestination: TAddress = dst
-  override def getProtocol: Transport = proto
-}
--- src/main/scala/example/bootstrap/BootstrapTimeout.scala
+++
@@ -1,5 +0,0 @@
-package example.bootstrap
-
-import se.sics.kompics.timer.{SchedulePeriodicTimeout, Timeout}
-
-case class BootstrapTimeout(spt: SchedulePeriodicTimeout) extends Timeout(spt) {}
