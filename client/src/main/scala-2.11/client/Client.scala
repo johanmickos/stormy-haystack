96b14896d39ca2b4647d3db759af70ac9d853718
Johan Mickos <jmickos@bu.edu>
2017-02-21 15:54:34 +0100

    Minor client cleanup

--- client/src/main/scala-2.11/client/Client.scala
+++ client/src/main/scala-2.11/client/Client.scala
@@ -6,7 +6,6 @@ import java.util.UUID
 import converters.TAddressConverter
 import ex.{TAddress, THeader, TMessage}
 import org.apache.commons.cli.{CommandLineParser, DefaultParser, Options}
-import overlay.PartitionLookupTable
 import se.sics.kompics.Kompics
 import se.sics.kompics.config.{Config, Conversions, ValueMerger}
 import se.sics.kompics.network.netty.serialization.Serializers
@@ -15,17 +14,17 @@ import serialization.PickleSerializer
 object Client {
 
     val NAC = TAddressConverter
+
     Conversions.register(TAddressConverter)
     Serializers.register(PickleSerializer, "pickleS")
-//    Serializers.register(classOf[PartitionLookupTable], "pickleS")
     Serializers.register(classOf[TAddress], "pickleS")
     Serializers.register(classOf[THeader], "pickleS")
     Serializers.register(classOf[TMessage[_]], "pickleS")
 
     def main(args: Array[String]): Unit = {
         val opts: Options = prepareOptions
-        val cliparser: CommandLineParser = new DefaultParser
-        val cmd = cliparser.parse(opts, args)
+        val clientParser: CommandLineParser = new DefaultParser
+        val cmd = clientParser.parse(opts, args)
 
         val cfg = Kompics.getConfig.asInstanceOf[Config.Impl]
         var self: TAddress = cfg.getValue("stormy.address", classOf[TAddress])
