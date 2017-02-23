25d8f3a7e4ca479f8d41183ef54ed1c7afcfae3a
Johan Mickos <jmickos@bu.edu>
2017-02-23 11:04:20 +0100

    Implement most of abortable sequence consensus

--- server/src/main/scala-2.11/components/Ports.scala
+++ server/src/main/scala-2.11/components/Ports.scala
@@ -4,10 +4,11 @@ import networking.NetAddress
 import se.sics.kompics.KompicsEvent
 import se.sics.kompics.sl._
 
+import scala.collection.mutable
+
 object Ports {
 
     case class PL_Deliver(src: NetAddress, payload: KompicsEvent) extends KompicsEvent
-
     case class PL_Send(dest: NetAddress, payload: KompicsEvent) extends KompicsEvent
 
     class PerfectLink extends Port {
@@ -16,7 +17,6 @@ object Ports {
     }
 
     case class Suspect(src: NetAddress) extends KompicsEvent
-
     case class Restore(src: NetAddress) extends KompicsEvent
 
     class EventuallyPerfectFailureDetector extends Port {
@@ -25,7 +25,6 @@ object Ports {
     }
 
     case class BEB_Deliver(src: NetAddress, payload: KompicsEvent) extends KompicsEvent
-
     case class BEB_Broadcast(payload: KompicsEvent) extends KompicsEvent
 
     class BestEffortBroadcast extends Port {
@@ -34,7 +33,6 @@ object Ports {
     }
 
     case class RB_Deliver(src: NetAddress, payload: KompicsEvent) extends KompicsEvent
-
     case class RB_Broadcast(payload: KompicsEvent) extends KompicsEvent
 
     class ReliableBroadcast extends Port {
@@ -43,7 +41,6 @@ object Ports {
     }
 
     case class CRB_Deliver(src: NetAddress, payload: KompicsEvent) extends KompicsEvent
-
     case class CRB_Broadcast(payload: KompicsEvent) extends KompicsEvent
 
     class CausalOrderReliableBroadcast extends Port {
@@ -53,11 +50,8 @@ object Ports {
 
 
     case class AR_Read_Request() extends KompicsEvent
-
     case class AR_Read_Response(value: Option[Any]) extends KompicsEvent
-
     case class AR_Write_Request(value: Any) extends KompicsEvent
-
     case class AR_Write_Response() extends KompicsEvent
 
     class AtomicRegister extends Port {
@@ -68,12 +62,21 @@ object Ports {
     }
 
     case class C_Decide(value: Any) extends KompicsEvent
-
     case class C_Propose(value: Any) extends KompicsEvent
 
     class Consensus extends Port {
         request[C_Propose]
         indication[C_Decide]
+    }
+
+    case class AC_Propose(value: Any) extends KompicsEvent
+    case class AC_Decide(value: Any) extends KompicsEvent
+    case object AC_Abort extends KompicsEvent
+
+    class AbortableConsensus extends Port {
+        request[AC_Propose]
+        indication[AC_Decide]
+        indication(AC_Abort)
     }
 
 
