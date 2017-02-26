a072c4a3995ce8a9d048cf2a3f8eafe3a32fd825
Johan Mickos <jmickos@bu.edu>
2017-02-26 12:07:18 +0100

    Update LUT to generate ranks, fix under-replication check

--- common/src/main/scala-2.11/stormy/overlay/PartitionLookupTable.scala
+++ common/src/main/scala-2.11/stormy/overlay/PartitionLookupTable.scala
@@ -6,20 +6,24 @@ import scala.collection.{mutable, _}
 
 
 class PartitionLookupTable(val replicationFactor: Int) {
+
     var partitions: mutable.MultiMap[Int, NetAddress] = new mutable.HashMap[Int, mutable.Set[NetAddress]] with mutable.MultiMap[Int, NetAddress]
+    var ranks: mutable.HashMap[NetAddress, Int] = mutable.HashMap()
 
     def generate(nodes: collection.immutable.Set[NetAddress]): Unit = {
         val numPartitions: Int = (nodes.size / replicationFactor).floor.toInt
         for ((node, i) <- nodes.zipWithIndex) {
             val partition: Int = i % numPartitions
             partitions.addBinding(partition, node)
+            val rank = i % replicationFactor + 1
+            ranks(node) = rank
         }
     }
 
     def isUnderReplicated: Boolean = {
         // Under-replicated? Should probably be caught in whomever is managing us
         // to trigger Stop Signal msg or lock the under-replicated partition
-        partitions.values.flatten.size % replicationFactor != 0
+        partitions.values.flatten.size < partitions.size * replicationFactor
     }
 
     def getNodes: Iterable[NetAddress] = {
